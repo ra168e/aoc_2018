744ba450460b2dfcf52f187441b07124aeabfd1d
ygurcan <yalin.gurcan@gmail.com>
2019-12-01 09:49:49 +0100

    Day 5 problem 1 started with recursion, runtime fail

---
+++ day5/aoc5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+string read_line(string filename)
+{
+    ifstream input;
+    input.open(filename);
+    string line;
+
+    if (input.is_open())
+    {
+        // Assuming we have just one line
+        getline(input, line);
+    }
+    return line;
+}
+
+string reduce(string line)
+{
+    string out, newline;
+    newline = line;
+    out = line;
+    for (int i = 1; i < line.length(); i++)
+    {
+        if (toupper(line[i - 1]) == toupper(line[i]) && abs(line[i -1] - line[i]) == 32)
+        {
+            newline.erase(i-1, 2);
+            out = reduce(newline);
+        }
+    }
+    return out;
+}
+
+int main()
+{
+    string example = "wWEbBpPeCQOoUuqc";
+    cout << reduce(example) << endl;
+}
+
+
